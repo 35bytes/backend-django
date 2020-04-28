921910d397dfe52590db0df42cf2cb372abf79f2
Karl Behrens <karlbehrensg@gmail.com>
2020-04-28 14:32:15 -0400

    Separando la vista hello-world

--- photogram/urls.py
+++ photogram/urls.py
@@ -15,13 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from django.http import HttpResponse
-
-def hello_world(request):
-    return HttpResponse('Hello, world!')
+from photogram import views
 
 urlpatterns = [
     
-    path('hello-world', hello_world)
+    path('hello-world', views.hello_world)
 
 ]
