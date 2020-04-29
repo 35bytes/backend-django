0e56cb8d6a5c465af265a7db18afc424fb3dd2c6
Karl Behrens <karlbehrensg@gmail.com>
2020-04-28 21:06:07 -0400

    Desplegando material visual a la vista posts

--- posts/views.py
+++ posts/views.py
@@ -2,6 +2,37 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+# Utilities
+from datetime import datetime
+
+posts = [
+    {
+        'name': 'Mont Blanc',
+        'user': 'Yésica Cortés',
+        'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
+        'picture': 'https://picsum.photos/200/200/?image=1036',
+    },
+    {
+        'name': 'Via Láctea',
+        'user': 'C. Vander',
+        'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
+        'picture': 'https://picsum.photos/200/200/?image=903',
+    },
+    {
+        'name': 'Nuevo auditorio',
+        'user': 'Thespianartist',
+        'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
+        'picture': 'https://picsum.photos/200/200/?image=1076',
+    }
+]
+
 def list_posts(request):
-    posts = [1, 2, 3, 4]
-    return HttpResponse(str(posts))
+    """List existing posts."""
+    content = []
+    for post in posts:
+        content.append("""
+            <p><strong>{name}</strong></p>
+            <p><small>{user} - <i>{timestamp}</i></small></p>
+            <figure><img src="{picture}"/></figure>
+        """.format(**post))
+    return HttpResponse('<br>'.join(content))
